f3ef1afd401a27d1d4e4bde02534ff8ce07847ee
daniel <dmendezp@live.com.mx>
2022-02-26 22:41:44 -0600

    variales javascript

---
+++ basicojavascritp/variables.js
@@ -0,0 +1,19 @@
+ var nombre = "daniel";
+
+ //declarar
+ var edad;
+
+ edad
+ //inicializar
+ edad = 30;
+
+ edad
+
+ var elemetos = ["computadora", "Celular"];
+ 
+ var persona = {
+     nombre: "daniel",
+     edad: 34
+ }
+
+ persona
