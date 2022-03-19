6b3f1185f1f7e61278bc78c70d6c175136483f6f
daniel <dmendezp@live.com.mx>
2022-03-19 04:05:31 -0600

    actualzamos CALCULAR MEDIANA

--- practico-javascritp/mediana.js
+++ practico-javascritp/mediana.js
@@ -15,7 +15,9 @@ function calcularMediaAritmetica(lista) {
        
    }
 
-const lista1 = [
+
+
+/*const lista1 = [
     100,
     200,
     300,
@@ -25,33 +27,51 @@ const lista1 = [
 
     
 ];
+*/
+function calcularMediana(lista1){
 
-const mitadLista1 = parseInt( lista1.length / 2);
+    lista1.sort();
+    
+    function compareNumbers(a, b) {
+        return a - b;
+      }
+    lista1.sort(compareNumbers);
 
-function esPar(numerito) {  
-    if(numerito % 2 === 0){
-        return true;
-    } else {
-        return false;
+
+    const mitadLista1 = parseInt( lista1.length / 2);
+
+    function esPar(numerito) {  
+        if(numerito % 2 === 0){
+            return true;
+        } else {
+            return false;
+        }
     }
+    
+    let mediana;
+    
+    if (esPar(lista1.length) ){
+        const elemento1 = lista1[mitadLista1 - 1];
+        const elemento2 = lista1[mitadLista1];
+    
+        const promedioElemento1y2 = calcularMediaAritmetica([
+            elemento1,
+            elemento2,
+        ])
+    
+        mediana = promedioElemento1y2;
+    
+    
+    
+    } else {
+        mediana = lista1[mitadLista1];
+    
+    }
+
+    return mediana;
+    
+    
+    return lista1;
+
 }
 
-let mediana;
-
-if (esPar(lista1.length) ){
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
-
-    const promedioElemento1y2 = calcularMediaAritmetica([
-        elemento1,
-        elemento2,
-    ])
-
-    mediana = promedioElemento1y2;
-
-
-
-} else {
-    mediana = lista1[mitadLista1];
-
-}
