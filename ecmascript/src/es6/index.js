26ef3aa2f4a4fe5e083fca4184932a0182ccfe2e
mendez <dmendezp@live.com.mx>
2022-03-27 16:52:31 -0600

    nueva clase htmly css

---
+++ ecmascript/src/es6/index.js
@@ -0,0 +1,13 @@
+function newfunction (name, age country){
+    var name = name || 
+    var age = age || 32;
+    var country = country || 'MX';
+    console.log(name, ager, country);
+}
+
+// es6
+
+function newFunction2(name = 'daniel', age= 32, country='Mexico') {
+    console.log(name,age,country);
+    
+}
