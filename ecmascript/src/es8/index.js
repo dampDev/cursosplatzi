870702fbd87b3943ee911b507b7b0777fd334aa5
daniel <dmendezp@live.com.mx>
2022-03-31 16:12:04 -0600

    es6 async await

--- ecmascript/src/es8/index.js
+++ ecmascript/src/es8/index.js
@@ -18,8 +18,35 @@ const values = Object.values(data1);
 console.log(values);
 console.log(values.length);
 
+//pading
+
 const string = 'hello';
 console.log(string.padStart(7,'hi'));
 console.log(string.padEnd(12,'----'));
 console.log('food'.padEnd(12,'----'));
 
+//async Await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ?setTimeout(()=> resolve ('Hello world'), 3000)
+        :reject(new Error('Test Error'))
+    })
+};
+const helloAsync = async() => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
